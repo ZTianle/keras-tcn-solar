99f6d178bf0f41e853070e374851240bf90f2392
ZTianle <tianlecsu@126.com>
2019-07-06 20:53:44 +0800

    Initial commit

---
+++ tasks/solar_forecast/main.py
@@ -0,0 +1,71 @@
+import keras
+from tcn import compiled_tcn
+from utils import data_generator
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+x, y = data_generator(seq_length=350)
+x_train, y_train = x[:int(len(y)*0.8),:,:] , y[:int(len(y)*0.8),:]
+x_test, y_test = x[int(len(y)*0.8):,:,:] , y[int(len(y)*0.8):,:]
+
+
+
+class PrintSomeValues(keras.callbacks.Callback):
+
+    def on_epoch_begin(self, epoch, logs={}):
+        print(f'x_test[0:1] = {x_test[0:1]}.')
+        print(f'y_test[0:1] = {y_test[0:1]}.')
+        print(f'pred = {self.model.predict(x_test[0:1])}.')
+
+
+def run_task():
+    model = compiled_tcn(return_sequences=False,
+                         num_feat=x_train.shape[2],
+                         num_classes=0,
+                         nb_filters=24,
+                         kernel_size=8,
+                         dilations=[2 ** i for i in range(9)],
+                         nb_stacks=1,
+                         max_len=x_train.shape[1],
+                         use_skip_connections=True,
+                         regression=True,
+                         dropout_rate=0.05)
+
+    print(f'x_train.shape = {x_train.shape}')
+    print(f'y_train.shape = {y_train.shape}')
+
+    psv = PrintSomeValues()
+
+    # Using sparse softmax.
+    # http://chappers.github.io/web%20micro%20log/2017/01/26/quick-models-in-keras/
+    model.summary()
+
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5,
+              callbacks=[psv], batch_size=256)
+
+    y_pred = model.predict(x_test)
+
+    pyplot.plot(history.history['loss'])
+    pyplot.plot(history.history['val_loss'])
+    pyplot.title('model train vs validation loss')
+    pyplot.ylabel('loss')
+    pyplot.xlabel('epoch')
+    pyplot.legend(['train', 'validation'], loc='upper right')
+    pyplot.show()
+
+    x_axix=range(len(y_test))
+    plt.plot(x_axix, y_test, color='green', label='test_ture')
+    plt.plot(x_axix, y_pred, color='red', label='test_tcn')
+    plt.legend()  # 显示图例
+    plt.xlabel('times')
+    plt.ylabel('solar')
+    plt.show()
+    test_mse_score, test_mae_score = model.evaluate(x_test, y_test, verbose=VERBOSE)
+    print('Test mse score:', test_mse_score)
+    print('Test mae score:', test_mae_score)
+
+    # 保存模型
+    model.save('my_model.h5')
+
+
+if __name__ == '__main__':
+    run_task()
---
+++ tasks/solar_forecast/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+
+
+
+def data_generator(seq_length):
+    """
+    Args:
+        seq_length: Length of the adding problem data
+        n: # of data in the set
+    """
+    csv_data = pd.read_csv('station1_new.csv')
+    csv_data_array = np.array(csv_data.tail(5479))
+    x_all = np.array(csv_data_array[:, 2: 9])
+    y_all = np.array(csv_data_array[:, 9])
+    x = np.zeros([len(y_all)-seq_length, seq_length, 7])
+    y = np.zeros([len(y_all)-seq_length, 1])
+    print(x_all.shape, len(y_all))
+
+    for i in range(len(y_all)-seq_length):
+        x[i, :, :] = x_all[i:i+seq_length, :]
+        y[i] = y_all[i+seq_length]
+   # print(x[-1,:,:], y[-1])
+
+    print(x.shape, y.shape)
+    return x, y
+
+
+if __name__ == '__main__':
+    csv_data = pd.read_csv('station1_new.csv')
+    csv_data_array = np.array(csv_data.tail(5479))
+    print(csv_data.shape)
+    print(csv_data_array.shape)
+    print(data_generator(seq_length=10))
