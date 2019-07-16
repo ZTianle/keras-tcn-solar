fe1e3a5aec176dfa5c2106f914415ea38d41d46a
ZTianle <tianlecsu@126.com>
2019-07-16 19:02:00 +0800

    decay_rbf

--- decayrbf/decay_rbf.py
+++ decayrbf/decay_rbf.py
@@ -1,2 +1,14 @@
 
-def decay_rbf(x, y):
+def decay_rbf(x, y, def_function):
+
+    if def_function=='Mexican Hat ':
+        fai=
+
+    elif def_function=='Morlet':
+
+    elif def_function=='Gaussian':
+
+    else:
+        raise Exception('wrong function')
+
+
